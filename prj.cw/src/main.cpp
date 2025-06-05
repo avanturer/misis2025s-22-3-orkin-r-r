13677deb3b2b3ab24ee565c5b7577b6aff74bbd6
Rodion <rodion.ork@yandex.ru>
2025-06-05 16:22:27 +0200

    delete qt

--- prj.cw/src/main.cpp
+++ prj.cw/src/main.cpp
@@ -6,17 +6,29 @@
  * @date June 2025
  */
 
-#include <QApplication>
-#include "mainwindow.h"
+// main.cpp
+#include <opencv2/opencv.hpp>
+#include "ShadowDetector.h" // Ваш класс для обработки
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <image_path>\n";
+        return -1;
+    }
 
-    MainWindow window;
-    window.setWindowTitle("Shadow Segmentation");
-    window.resize(1200, 800);
-    window.show();
+    cv::Mat input = cv::imread(argv[1]);
+    if (input.empty()) {
+        std::cerr << "Error loading image!\n";
+        return -1;
+    }
 
-    return app.exec();
+    ShadowDetector detector;
+    cv::Mat result = detector.processImage(input);
+
+    cv::imshow("Original", input);
+    cv::imshow("Shadow Segmentation", result);
+    cv::waitKey(0);
+    
+    return 0;
 }
+
