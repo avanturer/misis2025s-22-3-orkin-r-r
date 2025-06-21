a219c8d927fc2f07980ebaaf71bf95bcc0ad9dcf
avanturer <rodion.ork@yandex.ru>
2025-06-21 22:11:20 +0300

    fix debug

--- prj.cw/src/shadowledentifier.cpp
+++ prj.cw/src/shadowledentifier.cpp
@@ -21,14 +21,16 @@ cv::Mat ShadowLedentifier::processImage(const cv::Mat& input, const std::string&
     // 1. Маска по низкой яркости (V)
     cv::Mat v_mask;
     cv::threshold(hsv_channels[2], v_mask, value_threshold, 255, cv::THRESH_BINARY_INV);
-    // 2. Морфология (очистка и заполнение дыр)
+    cv::Mat v_mask_close, v_mask_open;
+    // 2. Морфология (close)
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(morph_kernel_size, morph_kernel_size));
-    cv::morphologyEx(v_mask, v_mask, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 2);
-    cv::morphologyEx(v_mask, v_mask, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 1);
-    // 3. Фильтрация по площади
+    cv::morphologyEx(v_mask, v_mask_close, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 2);
+    // 3. Морфология (open)
+    cv::morphologyEx(v_mask_close, v_mask_open, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 1);
+    // 4. Фильтрация по площади
     std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(v_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    cv::Mat filtered = cv::Mat::zeros(v_mask.size(), CV_8UC1);
+    cv::findContours(v_mask_open, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    cv::Mat filtered = cv::Mat::zeros(v_mask_open.size(), CV_8UC1);
     for (const auto& contour : contours) {
         double area = cv::contourArea(contour);
         if (area > min_shadow_area) {
@@ -47,10 +49,13 @@ cv::Mat ShadowLedentifier::processImage(const cv::Mat& input, const std::string&
                 std::cerr << "[ERROR] Failed to write: " << path << std::endl;
             }
         };
-        safe_write(outputPath + "/v_mask.jpg", v_mask);
-        safe_write(outputPath + "/filtered.jpg", filtered);
+        safe_write(outputPath + "/1_input.jpg", input);
+        safe_write(outputPath + "/2_v_mask.jpg", v_mask);
+        safe_write(outputPath + "/3_v_mask_close.jpg", v_mask_close);
+        safe_write(outputPath + "/4_v_mask_open.jpg", v_mask_open);
+        safe_write(outputPath + "/5_filtered.jpg", filtered);
         cv::Mat colored_result = createColoredMask(filtered, input);
-        safe_write(outputPath + "/final_overlay.jpg", colored_result);
+        safe_write(outputPath + "/6_final_overlay.jpg", colored_result);
     }
     return filtered;
 }
